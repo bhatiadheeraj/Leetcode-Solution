44d073ab121be50f876cd621346ac7563823de8a
Dheeraj Bhatia <dheerajb@buffalo.edu>
2021-08-25 04:40:20 -0400

    Update richestcustomerwealth.java

--- java/richestcustomerwealth.java
+++ java/richestcustomerwealth.java
@@ -1,1 +1,15 @@
-
+/* https://leetcode.com/problems/richest-customer-wealth/ */
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int result = 0;
+        int sum = 0;
+        for (int i =0; i<accounts.length ; i++) {
+            for(int j = 0; j < accounts[i].length; j++){
+                sum += accounts[i][j];
+            }
+            if(sum > result) result = sum;
+            sum = 0;
+        }
+        return result;
+    }
+}
