9a410db35015e3ff16671ef38980027357a110eb
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2025-07-15 12:58:25 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (31.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -2,23 +2,28 @@ class Solution:
     def isValid(self, s: str) -> bool:
         if len(s) % 2 != 0:
             return False
-
-        bracket_key = {
-            '{' : '}',
-            '[' : ']',
-            '(' : ')'
-        }
-        
-        stack = []
-        for index in range(len(s)):
-            item = s[index]
-            if item in bracket_key.keys():
-                stack.append(item)
-            else:
-                if stack == []:
-                    return False
-                if bracket_key[stack.pop()] != item:
-                    return False
-        if len(stack) != 0:
+        if s[0] == ']' or s[0] == '}' or s[0] == ')':
             return False
-        return True
+
+        map_bracket = {
+            '[' : ']',
+            '{' : '}',
+            '(' : ')' 
+        }
+
+        stack = []
+
+        #s = '{}'
+
+        for item in s:
+            if item in map_bracket.keys():
+                stack.append(item)
+            if item in map_bracket.values():
+                if len(stack) !=0:
+                    front_of_list = stack.pop()
+                    if map_bracket[front_of_list] != item:
+                            return False
+                else:
+                    return False
+        
+        return len(stack) == 0       
