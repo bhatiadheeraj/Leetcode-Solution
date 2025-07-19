6f9fc49aef7ac5a6879578765f3d889bac9740d4
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2025-07-18 23:41:25 -0500

    Time: 19 ms (15.38%) | Memory: 17.7 MB (97.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(s) != len(t) :
             return False
-        Counter(s)
-        return Counter(s) == Counter(t)
+        s_char_count = {}
+        t_char_count = {}
+
+        for index in range(0,len(s)):
+            char_s = s[index]
+            char_t = t[index]
+
+            if t_char_count.get(char_t, None) == None:
+                t_char_count[char_t] = 1
+            else:
+                t_char_count[char_t] +=1
+
+            if s_char_count.get(char_s,None) == None:
+                s_char_count[char_s] = 1
+            else:
+                s_char_count[char_s] +=1
+
+        for key in s_char_count.keys():
+            if s_char_count.get(key) != None and t_char_count.get(key) !=None and t_char_count.get(key) == s_char_count.get(key):
+                print("ok")
+            else:
+                return False
+        
+        return True
