609f68cb2089a5ad3462b03d39afc41c1c3fddce
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2025-07-25 16:01:00 -0500

    Time: 31 ms (10.64%) | Memory: 18.1 MB (34.73%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,5 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(magazine) < len(ransomNote):
+            return False
+
         charCount_magazine = {}
         for item in magazine:
             if charCount_magazine.get(item, 0) == 0 :
@@ -10,6 +13,8 @@ class Solution:
         charCount_ransomNote = {}
 
         for item in ransomNote:
+            if item not in charCount_magazine:
+                return False
             if charCount_ransomNote.get(item,0) == 0:
                 charCount_ransomNote[item] = 1
             else:
