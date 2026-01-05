7cb433d1a9e0ded540b85d965ecaf42ac1d94aec
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-05 01:27:38 -0800

    Time: 0 ms (100.00%) | Memory: 17.5 MB (87.45%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,22 +1,13 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        def fill(image: List[List[int]], sr: int, sc: int, orignalColor:int, color: int):
-            if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[0]):
-                return
-            
-            if image[sr][sc] != originalColor:
-                return
-            
-            image[sr][sc] = color
-
-            fill(image, sr-1,sc,originalColor,color)
-            fill(image, sr,sc+1,originalColor,color)
-            fill(image, sr,sc-1,originalColor,color)
-            fill(image, sr+1,sc,originalColor,color)
-
-        originalColor = image[sr][sc]
-        if originalColor == color:
-            return image
-
-        fill(image, sr,sc,originalColor,color)
+        if image[sr][sc] != color:
+            self.fill(image, sr,sc,image[sr][sc], color)
         return image
+
+    def fill(self, image: List[List[int]], sr: int, sc: int, oldcolor: int, newColor: int):
+        if sr >= 0 and sc >= 0 and sr < len(image) and sc < len(image[0]) and image[sr][sc] == oldcolor:
+            image[sr][sc] =newColor
+            self.fill(image, sr+1,sc, oldcolor, newColor)
+            self.fill(image, sr-1,sc,oldcolor, newColor)
+            self.fill(image, sr,sc+1,oldcolor, newColor)
+            self.fill(image, sr,sc-1,oldcolor, newColor)
