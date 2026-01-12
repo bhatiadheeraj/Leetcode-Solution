e1d5c9e0739216a590ac7ccbbc9bccab2174a2f5
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-12 01:42:46 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.62%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,28 +1,25 @@
 class MyQueue:
-    #stack = filo
-    #queue is firfo
-
     stack = []
-    stackTransfer = []
+    selfTransfer = []
     def __init__(self):
         self.stack = []
         self.stackTransfer = []
-
     def push(self, x: int) -> None:
         self.stack.append(x)
 
     def pop(self) -> int:
-        while(len(self.stack)) :
+        while(len(self.stack)):
             self.stackTransfer.append(self.stack.pop())
         ret = self.stackTransfer.pop()
-        while(len(self.stackTransfer)) :
+        while(len(self.stackTransfer)):
             self.stack.append(self.stackTransfer.pop())
         return ret
+
     def peek(self) -> int:
         return self.stack[0]
 
     def empty(self) -> bool:
-        return len(self.stack) == 0
+        return len(self.stack) == 0 
 
 
 # Your MyQueue object will be instantiated and called as such:
