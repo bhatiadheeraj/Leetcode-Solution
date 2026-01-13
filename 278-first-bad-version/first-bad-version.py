3e9b90ccb76273e7466f0e62f8d630a364b5c4e6
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-13 01:20:30 -0800

    Time: 40 ms (35.75%) | Memory: 19.2 MB (7.57%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -3,13 +3,17 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        right = n
-        left = 1
+        #intuition is keep changing the left and right as the firstbadversion found means everything to right of it is bad
 
-        while left < right :
-            mid = (left + right ) // 2
-            if isBadVersion(mid):
+        left = 1
+        right = n 
+
+        while left < right:
+            mid = (left+right) // 2 
+            if isBadVersion(mid) == True:
                 right = mid
-            else: 
+            else:
                 left = mid + 1
-        return right
+        
+        return left
+        
