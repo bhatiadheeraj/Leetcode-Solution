ec1b7e939ed82af786b9a907654c413c49e86f53
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-13 00:54:04 -0800

    Time: 37 ms (7.70%) | Memory: 19.6 MB (10.39%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,29 +1,22 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        if len(magazine) < len(ransomNote):
-            return False
-
-        charCount_magazine = {}
+        char_to_count_ransomNote = {}
+        char_to_count_magazine = {}
+        
         for item in magazine:
-            if charCount_magazine.get(item, 0) == 0 :
-                charCount_magazine[item] = 1
-            else:
-                charCount_magazine[item] +=1
+            if char_to_count_magazine.get(item,0):
+                char_to_count_magazine[item] += 1
+            else :
+                char_to_count_magazine[item] = 1
         
-        charCount_ransomNote = {}
-
         for item in ransomNote:
-            if item not in charCount_magazine:
-                return False
-            if charCount_ransomNote.get(item,0) == 0:
-                charCount_ransomNote[item] = 1
-            else:
-                charCount_ransomNote[item] += 1
-            if charCount_ransomNote[item] > charCount_magazine.get(item, 0):
+            if char_to_count_ransomNote.get(item,0):
+                char_to_count_ransomNote[item] += 1
+            else :
+                char_to_count_ransomNote[item] = 1
+        
+        for key in char_to_count_ransomNote.keys():
+            if char_to_count_magazine.get(key,0) < char_to_count_ransomNote.get(key,0):
                 return False
         
-
         return True
-
-
-        
