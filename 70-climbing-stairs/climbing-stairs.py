1875147d6e07578cdd53f6f52d6dfed16f64c851
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-15 23:10:23 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.37%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,9 +2,8 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         ways = [1, 1, 2]
 
-
         for index in range(3, n+1):
-            print(index, ways[index -1 ], ways[index-2])
-            ways.append(ways[index - 1] + ways[index - 2])
+            ways.append(ways[index-1] + ways[index -2])
+        
         return ways[n]
         
