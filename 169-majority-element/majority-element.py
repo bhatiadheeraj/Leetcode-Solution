93f1b92fe8ac2e1a807e5813acb3fd512cdc71b9
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-16 00:28:43 -0800

    Time: 7 ms (57.90%) | Memory: 21.4 MB (9.02%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,18 +1,17 @@
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter_tracker = {}
-        for item in nums:
-            if counter_tracker.get(item,0) == 0:
-                counter_tracker[item] = 1
-            else:
-                counter_tracker[item] += 1
+        threshold = floor(len(nums) // 2)
+        dict_vals = Counter(nums)
+
+        maxVal = -1
+        ret = 0
+
+        print(dict_vals, threshold)
+        for key,val in dict_vals.items():
+            if val >= threshold and val > maxVal:
+                maxVal = val
+                ret = key
+
         
-        maxCount = 0
-        num = 0
-        threshold = math.floor(len(nums)/2)
-        print(counter_tracker, threshold)
-        for key,val in counter_tracker.items():
-            if val >= threshold and val > maxCount:
-                num = key
-                maxCount = val
-        return num
+        return ret 
