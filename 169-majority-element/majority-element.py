f46c79e9fc4ced8123185c58d65fda9acabb562f
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-21 05:05:19 -0800

    Time: 7 ms (58.12%) | Memory: 21.3 MB (13.38%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
+        threshold = len(nums) // 2 #floor integer divsion
         count = 0
         res = 0
-        for item in nums:
+        for digit in nums:
             if count == 0:
                 count = 1
-                res = item
-            elif res == item :
-                count +=1
-            else:
+                res = digit
+            elif digit !=res:
                 count -= 1
+            else:
+                count += 1
         return res
+
+
+
