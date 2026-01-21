a9589ea8d31cdb79e25884e32bccf5080f2fdaf0
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-21 04:59:31 -0800

    Time: 0 ms (100.00%) | Memory: 19.2 MB (24.34%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,14 +1,13 @@
 from collections import Counter
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        char_count = Counter(s)
+        count = 0 
 
-        count = 0
-
-        for item in char_count.values():
-            count += item // 2 * 2
-
-        if count < len(s) : 
-            count += 1
-
+        for item in Counter(s).values():
+            count += item // 2 * 2 
+        
+        if count < len(s):
+            count +=1
+        
         return count
+        
