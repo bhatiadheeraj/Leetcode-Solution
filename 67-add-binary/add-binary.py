6c6096582951a51bf33f5fce1e65f74fb3337070
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-23 22:21:46 -0800

    Time: 3 ms (46.21%) | Memory: 19.2 MB (29.27%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,36 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if not a:
-            return b if b else "0"
-        if not b:
-            return a
-  
-        i = len(a) - 1
+        res = []
+        i = len(a) - 1 
         j = len(b) - 1
+
         carry = 0
-        result = []
 
-        # while i>= 0 and j >= 0 or carry:
-        #     bit_a = int(a[i]) if i>=0 else 0 
-        #     bit_b = int(b[j]) if j >=0 else 0
-        #     total = bit_a + bit_b + carry 
-        #     result.append(str(total % 2))
-        #     carry = total // 2
-        #     i -= 1
-        #     j -= 1
+        while i >= 0 or j >=0 or carry:
+            int_a_bit = int(a[i]) if i >=0 else 0
+            int_b_bit = int(b[j]) if j >=0 else 0
+            sum = int_a_bit + int_b_bit + carry 
+            carry = (sum // 2)
+            res.append(str(sum % 2))
 
-        # return ''.join(reversed(result))
-
-
-        while i >= 0 or j >= 0 or carry:
-            bit_a = int(a[i]) if i >= 0 else 0
-            bit_b = int(b[j]) if j >= 0 else 0
-          
-            total = bit_a + bit_b + carry
-            result.append(str(total % 2))
-            carry = total // 2
-          
-            i -= 1
-            j -= 1
-      
-        return ''.join(reversed(result))
+            i -=1
+            j -=1
+        return "".join(reversed(res))
