fb5acc27b4d3d092de996586ec2b7cb7a1c7644f
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-24 11:10:33 -0800

    Time: 5 ms (8.26%) | Memory: 19.4 MB (15.73%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,18 +1,19 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         res = []
-        i = len(a) - 1 
+        i = len(a) - 1
         j = len(b) - 1
-
         carry = 0
 
-        while i >= 0 or j >=0 or carry:
-            int_a_bit = int(a[i]) if i >=0 else 0
-            int_b_bit = int(b[j]) if j >=0 else 0
-            sum = int_a_bit + int_b_bit + carry 
-            carry = (sum // 2)
+        while i >=0 or j >= 0 or carry:
+            int_a = int(a[i]) if i>=0 else 0
+            int_b = int(b[j]) if j >=0 else 0
+
+            sum = int_a + int_b + carry
+            carry = sum // 2 
             res.append(str(sum % 2))
 
-            i -=1
-            j -=1
+            i-=1
+            j-=1
+        
         return "".join(reversed(res))
