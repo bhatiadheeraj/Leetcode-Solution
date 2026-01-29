fb5f23fc494e7335c46ebf5aff12ffa7a9236ba5
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-01-29 04:42:40 -0800

    Time: 290 ms (5.01%) | Memory: 24.6 MB (27.77%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,19 +1,13 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        point_distance_tuples = []
-        for index in range(0, len(points)):
-            point_distance_tuples.append((points[index], self.distanceToOrigin(points[index][0], points[index][1])))
-
-        ret = sorted(point_distance_tuples, key = lambda x: x[1])
-        count = 0
         res = []
-        for item in ret:
-            if count < k:
-                res.append(item[0])
-                count +=1
-        return res
-
-    def distanceToOrigin(self, x, y):
-        # distance = (0-x)^2 + (0-y)^2
-        # return sqrt(abs(distance))
-        return math.dist([0,0],[x,y])
+        for point in points:
+            res.append((point, self.calDistance(point[0],point[1])))
+        partial = sorted(res, key = lambda point: point[1])[:k]
+        cleanRes = []
+        print(partial, sorted(res))
+        for item in partial:
+            cleanRes.append(item[0])
+        return cleanRes
+    def calDistance(self,x,y):
+        return math.dist((0,0), (x,y))
