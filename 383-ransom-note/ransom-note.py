ad2c785652bf7dc38b48d87ac1d4f3c9286c8d79
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-02-09 01:22:24 -0800

    Time: 11 ms (91.38%) | Memory: 19.7 MB (45.09%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,16 +1,13 @@
 from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        #each letter in magazine only be used once in ransomNote
-        if len(magazine) < len(ransomNote):
-            return False
-        # each unique char in magazine should be present atleast equally or more than ransomeNote
+        count_mag = Counter(magazine)
 
-        count_char_ransom_note = Counter(ransomNote)
-        count_char_magazine = Counter(magazine)
-        
-        for key,val in count_char_ransom_note.items(): 
-            if count_char_magazine.get(key, 0) < val:
+        for index in range(0, len(ransomNote)):
+            char = ransomNote[index]
+            if count_mag.get(char, 0) >0:
+                count_mag[char] -= 1
+            else:
                 return False
-        return True
         
+        return True
