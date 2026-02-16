de1255347e47f7dec4130637e6590aca6ccbd184
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-02-15 23:36:25 -0800

    Time: 103 ms (84.40%) | Memory: 21.3 MB (60.95%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,25 +1,26 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        queue = deque()
-        rows, cols = len(mat), len(mat[0])
+        rows = len(mat)
+        cols = len(mat[0])
 
+        explore = deque()
         for row in range(rows):
             for col in range(cols):
-                if mat[row][col] ==0:
-                    queue.append((row,col))
+                if mat[row][col] != 0:
+                    mat[row][col] = -1
                 else:
-                    mat[row][col] = float('inf')
-        print(mat)
-        directions = [(-1,0), (0,1), (1,0), (0,-1)]
+                    explore.append((row,col))
+        
+        directions = [(1,0), (0,1), (0,-1), (-1,0)]
 
-        while queue:
-            i,j = queue.popleft()
-            print(mat[i][j], i,j)
-            for col,row in directions:
+        while explore:
+            i,j = explore.popleft()
+            for row, col in directions:
                 next_row = i + row
                 next_col = j + col
-                if 0<= next_row < rows and 0 <= next_col < cols and mat[next_row][next_col] == float('inf'):
+
+                if (0 <= next_row < rows and 0 <= next_col < cols and mat[next_row][next_col] == -1):
                     mat[next_row][next_col] = mat[i][j] + 1
-                    queue.append((next_row, next_col))
-        
+                    explore.append((next_row,next_col))
         return mat
+            
