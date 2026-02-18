8b671baa0af8dd332dd9e5beadaf4a0023fb153d
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-02-17 21:28:16 -0800

    Time: 98 ms (89.13%) | Memory: 21.4 MB (62.77%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -3,24 +3,24 @@ class Solution:
         rows = len(mat)
         cols = len(mat[0])
 
-        explore = deque()
+        queue = deque()
         for row in range(rows):
             for col in range(cols):
-                if mat[row][col] != 0:
+                if mat[row][col] == 1:
                     mat[row][col] = -1
                 else:
-                    explore.append((row,col))
-        
-        directions = [(1,0), (0,1), (0,-1), (-1,0)]
+                    queue.append((row,col))
+        directions = [(-1,0), (1,0), (0,1), (0,-1)]
 
-        while explore:
-            i,j = explore.popleft()
+        while queue:
+            i,j = queue.popleft()
             for row, col in directions:
-                next_row = i + row
+                next_row = i+row
                 next_col = j + col
 
+                #safe check limits
                 if (0 <= next_row < rows and 0 <= next_col < cols and mat[next_row][next_col] == -1):
                     mat[next_row][next_col] = mat[i][j] + 1
-                    explore.append((next_row,next_col))
+                    queue.append((next_row,next_col))
+       
         return mat
-            
