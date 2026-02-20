6326fe140856ac3f842b7071fa3bcfbdd304d001
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-02-19 21:17:35 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (47.60%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -3,15 +3,13 @@ class Solution:
         oldColor = image[sr][sc]
         if oldColor == color:
             return image
-        self.fill(image, sr,sc, oldColor, color)
+        self.fill(image,sr,sc,oldColor, color)
         return image
-    def fill(self,image,sr,sc,oldColor,color):
-        if sr >= 0 and sr < len(image) and sc >= 0 and sc < len(image[0]) and image[sr][sc] == oldColor:
-            image[sr][sc] = color
-            self.fill(image,sr+1, sc, oldColor, color)
-            self.fill(image,sr-1, sc, oldColor, color)
-            self.fill(image,sr, sc+1, oldColor, color)
-            self.fill(image,sr, sc-1, oldColor, color)
-        else:
-            return
-        
+    def fill(self, image, sr, sc,oldColor, newColor):
+        if sr >= 0 and sr < len(image) and sc >=0 and sc < len(image[0]) and image[sr][sc] == oldColor:
+            image[sr][sc] = newColor
+            self.fill(image, sr+1, sc, oldColor, newColor)
+            self.fill(image, sr-1, sc, oldColor, newColor)
+            self.fill(image, sr, sc+1, oldColor, newColor)
+            self.fill(image, sr, sc-1, oldColor, newColor)
+
