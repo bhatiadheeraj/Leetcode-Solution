86eba405bdba1123733a875efd2446e62278f195
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-03-05 21:20:55 -0800

    Time: 7 ms (57.69%) | Memory: 21.4 MB (20.12%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,7 @@
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        element = nums[0]
-        count = 1
-        for index in range(1, len(nums)):
-            if nums[index] == element:
-                count +=1
-            elif nums[index] != element:
-                count -=1 
-                if count ==0:
-                    element = nums[index]
-                    count +=1 
-        return element
+        dict_num = Counter(nums)
+        for key, value in dict_num.items():
+            if value > math.floor(len(nums) // 2):
+                return key
