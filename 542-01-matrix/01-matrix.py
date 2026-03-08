d179c9940e3066b96a46e068ae9112be6aa7e3be
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-03-08 11:27:25 -0700

    Time: 179 ms (20.12%) | Memory: 21 MB (96.71%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,23 +1,28 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        #prepare the matrix and bfs :) 
+        queue = deque()
+        #mark the ones for bfs search and put the others in queue for exploration 
         rows = len(mat)
         cols = len(mat[0])
 
-        queue = deque()
         for row in range(rows):
             for col in range(cols):
                 if mat[row][col] == 0:
-                    queue.append([row,col])
+                    queue.append((row,col))
                 else:
                     mat[row][col] = float('-inf')
-
+        
         directions = [(0,-1), (1,0), (0,1), (-1,0)]
+
         while queue:
-            i, j = queue.popleft()
+            i,j = queue.popleft()
             for direction in directions:
-                neighbor_x,neighbor_y = i + direction[0], j +direction[1]
-                if 0 <= neighbor_x < rows and 0 <= neighbor_y < cols and mat[neighbor_x][neighbor_y] == float('-inf'):
-                    mat[neighbor_x][neighbor_y] = mat[i][j] + 1
-                    queue.append((neighbor_x,neighbor_y ))
-                
-        return mat
+                next_row = i + direction[0]
+                next_col = j + direction[1]
+
+                if 0 <= next_row < rows and 0 <= next_col < cols and mat[next_row][next_col] == float('-inf'):
+                    mat[next_row][next_col] = mat[i][j] + 1
+                    queue.append((next_row, next_col))
+
+        return mat 
