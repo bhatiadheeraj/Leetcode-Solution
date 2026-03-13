ac00c5f0aa96a6109fe38002ac192fcfd243d926
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-03-12 19:18:47 -0700

    Time: 171 ms (23.08%) | Memory: 21.1 MB (94.06%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,28 +1,24 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        #prepare the matrix and bfs :) 
-        queue = deque()
-        #mark the ones for bfs search and put the others in queue for exploration 
+        q = deque()
         rows = len(mat)
         cols = len(mat[0])
 
         for row in range(rows):
             for col in range(cols):
-                if mat[row][col] == 0:
-                    queue.append((row,col))
+                if mat[row][col] !=0:
+                    mat[row][col] = float('inf')
                 else:
-                    mat[row][col] = float('-inf')
-        
-        directions = [(1,0), (-1,0), (0,1), (0, -1)]
+                    q.append((row,col))
 
-        while queue:
-            i,j = queue.popleft()
+        directions = [(-1,0), (0, 1), (1 , 0), (0,-1)]
+        while q:
+            i,j = q.popleft()
             for direction in directions:
-                next_row = i + direction[0]
-                next_col = j + direction[1]
-
-                if next_row >=0 and next_row < rows and next_col >=0 and next_col < cols and mat[next_row][next_col] == float('-inf'):
-                    mat[next_row][next_col] = mat[i][j] + 1
-                    queue.append((next_row, next_col))
-
-        return mat 
+                n_row = i + direction[0]
+                n_col = j + direction[1]
+                if n_row >= 0 and n_row < rows and 0 <= n_col < cols and mat[n_row][n_col] == float('inf'):
+                    mat[n_row][n_col] = mat[i][j] + 1 
+                    q.append((n_row,n_col))
+        
+        return mat
