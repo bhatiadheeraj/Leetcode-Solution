808f9d62aa1387258167c7481bc8aa8bcbf074e9
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2026-03-25 21:34:48 -0700

    Time: 6 ms (46.81%) | Memory: 19.4 MB (14.27%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,21 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_to_int = {
+        SYMBOL_TO_VAL = {
             'I' : 1,
             'V' : 5,
-            'X' : 10, 
+            'X' : 10,
             'L' : 50,
             'C' : 100,
             'D' : 500,
             'M' : 1000
         }
 
-        result = 0 
+        result = 0
         last_item = 0
-        for item in s:
-            if roman_to_int.get(last_item,0) < roman_to_int.get(item,0):
-                result -= 2* roman_to_int.get(last_item,0)
-            result += roman_to_int.get(item,0)
-            last_item = item
-        
+        for index, item in enumerate(s): # 1000 1100 2000 2010 2111
+            value = SYMBOL_TO_VAL[item]
+            if last_item < value:
+                result -= last_item 
+                result += value-last_item
+            else:
+                result += value
+            last_item = value
         return result
